f21b78fdbe64434448f551e8c0907dccfd712d85
Carl Smith <caat.salam@gmail.com>
2016-07-28 04:27:28 +0100

    Tidied up the top level update script.

--- update.py
+++ update.py
@@ -1,11 +1,41 @@
+""" Update Script
+============================================================ OQ == GPL3 2016 ==
+
+When using this (currently limited) toolset from the command line, you will
+always run this file as the top-level script. Its job is converting the
+translation source code into HTML (and other formats eventually).
+
+The overall process has three stages. There are 114 (one pre surah) source
+files, which are edited by hand, and use a simple markup format. They are
+parsed into JSON AST structs that are written to 114 JSON text files. The
+single HTML page (index.html) is generated from those JSON files.
+
+    114 source files -> 114 JSON AST files -> 1 HTML file
+
+Other output formats will be added once the translation is complete.
+
+You can currently do three things with this script:
+
+    1: $ python update.py json
+    2: $ python update.py book
+    3: $ python update.py
+
+Case 1 updates the JSON, Case 2 updates the HTML, and Case 3 updates both the
+JSON and the HTML.
+
+In case 1 and 2, the script only updates the JSON for source files that have
+been edited since the last time the script was executed. In case 1 and 3, the
+HTML is is regenerated from scratch, based on the current state of the JSON
+AST files."""
+
 import os, sys, json, time, datetime
-from html import book
 from tell import tell
+from html import update as html
 
 def last_modified(path):
 
-    """This function takes a path to a file, and returns the last
-    modified date. It is used by `update_json` to know which files
+    """This function takes a path to a file, and returns the date that it was
+    last modified. This function is used by `update_json` to know which files
     have been modified since the last time it was called."""
 
     date = os.path.getmtime(path)
@@ -14,77 +44,72 @@ def last_modified(path):
 
 def update_timestamp():
 
-    """This function updates the timestamp (stored in a file named
-    timestamp.data), then returns the old timestamp. It is used by
-    `update_json` to know which files have been modified since the
-    last time `update_json` was called."""
+    """This function updates the timestamp, which is stored in a file named
+    timestamp.data, then returns the old timestamp. It is used by `update_json`
+    to know which files have been modified since the last time `update_json`
+    was called."""
 
     with open("timestamp.data", "r+") as file:
-
         old = float(file.read())
-
-        now = datetime.datetime.now()
-        now = time.mktime(now.timetuple())
-
+        now = time.mktime(datetime.datetime.now().timetuple())
         file.seek(0)
         file.write(str(now))
         file.truncate()
-
         return old
 
 def parse(source):
 
-    """This function parses a string of source, and returns a JSON AST,
-    as a hash. It breaks the source into a list of blocks, which are
-    paragraphs. Each block contains one or more verses.
-    """
+    """This function parses a string of source - the content from a source
+    file - which represents a surah. The function returns an AST for the
+    surah. The AST for each surah is structured as a list of paragraphs,
+    and each paragraph is structured as a list of verses. Each verse is
+    a hash with three properties:
+
+        quran: String # the text of the verse (with one trailing space)
+        surah: Number # the surah number
+        verse: Number # the verse number
+
+    Note: The unindexed Bismillahs have `0` for their `verse` property."""
 
     def blockify(source):
 
-        """This is the first step, where the source is broken into blocks.
-        The output is a list of strings; each string is a block. Internal
-        newlines are removed in the process."""
+        """This is the first step, where the source is broken into paragraphs,
+        based on blank lines in the source. The output is a list of strings.
+        Each string is a paragraph. Newlines (with any trailing whitespace)
+        inside paragraphs are converted to single spaces."""
 
-        blocks = [""]
-
-        for line in source.split("\n"):
-
+        paragraphs = [""]
+        for line in source.strip().split("\n"):
             line = line.strip()
+            if line: paragraphs[-1] += line + " "
+            elif paragraphs[-1]: paragraphs.append("")
 
-            if line: blocks[-1] += line + " "
-            elif blocks[-1]: blocks.append("")
-
-        return [ block for block in blocks if block ]
+        return paragraphs
 
     def subparse(block):
 
-        """This function parses a single block of source, as returned by
-        blockify. It finds the verses, and creates an AST that represents
-        one paragraph in the translation.
+        """This function parses a single paragraph of source, as returned by
+        the `blockify` function. This finds the individual verses within the
+        given paragraph. It returns an AST for the paragraph, as previously
+        described.
+
+        TODO: Validate the input based on the AST.
         """
 
         verses = []
         context = None
-
         for char in block:
 
             if char == "[":
-
                 if verses: verses[-1]["quran"] = verses[-1]["quran"].strip()
-
                 verses.append({"surah": "", "verse": "", "quran": ""})
                 context = "surah"
-
             elif char == ":" and context == "surah":
-
                 verses[-1]["surah"] = int(verses[-1]["surah"])
                 context = "verse"
-
             elif char == "]":
-
                 verses[-1]["verse"] = int(verses[-1]["verse"])
                 context = "quran"
-
             else: verses[-1][context] += char
 
         verses[-1]["quran"] = verses[-1]["quran"].strip()
@@ -94,14 +119,13 @@ def parse(source):
 
 def update_json(shell=False):
 
-    """This function cycles through the 115 source files, looking for the
-    ones that have been modified since the last time it was executed. The
-    function updates the JSON where necessary to keep all the input and
-    output files in sync."""
+    """This function cycles through the 114 source files, looking for any
+    that have been modified since the last time it was executed. The function
+    updates the JSON where necessary to bring the JSON AST files into sync
+    with the source."""
 
     surahs_modified = 0
     last_update = update_timestamp()
-
     if shell: tell.info("Updating JSON AST files.")
 
     for surah in range(1, 115):
@@ -111,41 +135,37 @@ def update_json(shell=False):
         if last_modified(source_path) > last_update:
 
             if shell:
-
-                tell.info("Rebuilding Surah {}.".format(surah))
+                tell.info("Rebuilding Surah `{}`.".format(surah))
                 surahs_modified += 1
 
             with open(source_path) as source_file:
-
                 with open("json/surah{}.json".format(surah), "w") as json_file:
-
                     data = parse(source_file.read())
                     json_file.write(json.dumps(data))
 
     if shell:
-
         if surahs_modified: tell.done("The JSON files are now up to date.")
         else: tell.info("No edits were found in the source files.")
 
     return surahs_modified
 
-def update_book(shell=False):
+def update_html(shell=False):
 
-    tell.info("Rebuilding the HTML book.")
-    book.generate()
-    tell.done("HTML book updated.")
+    """The function regenerates the HTML version of the translation. It
+    assumes the JSON is upto date."""
+
+    if shell: tell.info("Rendering the HTML.")
+    html.generate()
+    if shell: tell.done("Updated `html/index.html`.")
 
 if __name__ == "__main__":
 
     try: arg = sys.argv[1]
     except IndexError: arg = None
 
-    if arg is None:
-
-        if update_json(True): update_book(True)
-
+    if arg is None: update_json(True) and update_html(True)
     elif arg == "json": update_json(True)
-    elif arg == "book": update_book(True)
+    elif arg == "book": update_html(True)
     else:
 
         tell.fail("Unrecognised arguments: `{}`".format(sys.argv[1:]))
